b04b19782fac342ed8dae37a5e1ec694aaf2fd5d
Rahul Prajapati <rkumarprajapati14@gmail.com>
2024-10-19 20:24:19 +0530

    Added code and test case for new delimiter scenarios

--- string_calculator.js
+++ string_calculator.js
@@ -1,6 +1,6 @@
 class StringCalculator{
     constructor(){}
-
+    
     // Function to implement add operation
     add(numbers){
         try{
@@ -9,11 +9,25 @@ class StringCalculator{
                 return 0;
             }
             let sum = 0;
-            //Splitting string if \n encountered
-            for(let line of numbers.split('\n')){
-                //Splitting line if , delimiter encountered
-                for(let number of line.split(',')){
-                    sum += parseInt(number)
+            //Check if string starts with //
+            if(numbers.startsWith('//')){
+                let first_new_line_index = numbers.indexOf('\n');
+                //Fetching new delimiter
+                let new_delimiter = numbers.slice(2, first_new_line_index);
+                numbers = numbers.slice(numbers.indexOf('\n') + 1);
+                for(let line of numbers.split('\n')){
+                    for(let number of line.split(new_delimiter)){
+                        sum += parseInt(number)
+                    }
+                }
+            }
+            else{
+                //Splitting string if \n encountered
+                for(let line of numbers.split('\n')){
+                    //Splitting line if , delimiter encountered
+                    for(let number of line.split(',')){
+                        sum += parseInt(number)
+                    }
                 }
             }
             return sum;
--- string_calculator.test.js
+++ string_calculator.test.js
@@ -20,3 +20,13 @@ test("Test Case 3 -> If string contains comma seperated values", () => {
 test("Test Case 4 -> If string contains more than one line", () => {
     expect(calculator.add("1,2,3\n4")).toBe(10)
 });
+
+// Test Case 5 -> If string contains delimiter other than comma(,)
+test("Test Case 5 -> If string contains delimiter other than comma(,)", () => {
+    expect(calculator.add("//;\n1;2;3")).toBe(6)
+});
+
+// Test Case 6 -> If string contains delimiter other than comma(,) and have more than one lines
+test("Test Case 6 -> If string contains delimiter other than comma(,) and have more than one lines", () => {
+    expect(calculator.add("//;\n1;2;3\n4;5\n6;7;8")).toBe(36)
+});
